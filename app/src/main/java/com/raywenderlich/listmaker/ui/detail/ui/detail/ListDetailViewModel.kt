0fc73d326c1816848ca56dffca9e5bd8164c195b
TitaniusAnglesmith1 <wnettl1@live.spcollege.edu>
2022-08-31 08:15:55 -0400

    Initial commit

---
+++ app/src/main/java/com/raywenderlich/listmaker/ui/detail/ui/detail/ListDetailViewModel.kt
@@ -0,0 +1,15 @@
+package com.raywenderlich.listmaker.ui.detail.ui.detail
+
+import androidx.lifecycle.ViewModel
+import models.TaskList
+
+class ListDetailViewModel() : ViewModel() {
+    lateinit var onTaskAdded: (() -> Unit)
+    lateinit var list: TaskList
+
+    fun addTask(task: String) {
+        list.tasks.add(task)
+        onTaskAdded.invoke()
+    }
+
+}
